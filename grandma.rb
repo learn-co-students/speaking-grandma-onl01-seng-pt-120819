13d1df51e9d75a735b3f83070e161e5cfba5b0fc
dwane richards <dwanerich@gmail.com>
2019-12-11 03:39:17 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,18 @@
 def speak_to_grandma(phrase)
 
-  if phrase.upcase == phrase && phrase != "I LOVE YOU GRANDMA!"
-    return "NO, NOT SINCE 1938!"
-  elsif
-    phrase != phrase.upcase
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif
     phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"  
+  elsif
+    phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
   end
 
 end
 
-speak_to_grandma("I LOVE YOU")
+speak_to_grandma("hey old lady")
 
 
 
